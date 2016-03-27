74f87705f954750d012e54d43321d169dc6c82c3
Paulo Henrique <p.h.moreirabarbosa@hotmail.com>
2016-03-27 18:38:00 -0300

    Cadastro Java FX Parte 4

--- AddressApp/src/ch/makery/address/MainApp.java
+++ AddressApp/src/ch/makery/address/MainApp.java
@@ -10,6 +10,7 @@ import javafx.collections.ObservableList;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -94,6 +95,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        // Set the application icon.
+        this.primaryStage.getIcons().add(new Image("file:resources/images/address_book_32.png"));
 
         initRootLayout();
 
